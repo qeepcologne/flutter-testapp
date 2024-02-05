267a2d577edc28d5282258e92f46c6b8571ce990
Bernd Wahlen <bernd.wahlen@k2interactive.de>
2024-02-05 11:13:15 +0100

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     debugImplementation("com.example.client.flutter_module:flutter_release:1.0")
     releaseImplementation("com.example.client.flutter_module:flutter_release:1.0")
